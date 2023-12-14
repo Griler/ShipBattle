c505c2d0f750df51ccfa8360782a5d3050337c4f
Griler <thienloc662001@gmail.com>
2023-12-14 23:45:34 +0700

    Name event file

---
+++ assets/Script/NAME_EVENT.js
@@ -0,0 +1,7 @@
+const eventName= {
+    FINISH_SHOOT:"FINISH_SHOOT",
+    RESET_TURN:"RESET_TURN",
+    CHANGE_SCENE:"CHANGE_SCENE",
+    COLD_DOWN_CLOCK:"COLD_DOWN_CLOCK",
+}
+module.exports = eventName;
