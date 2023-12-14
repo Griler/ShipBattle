56c52c331aef3d2bf8726c7b9099f9c2b5529dd4
Griler <thienloc662001@gmail.com>
2023-12-14 23:48:20 +0700

    config file clean code

--- assets/Script/FindTileInMap.js
+++ assets/Script/FindTileInMap.js
@@ -8,20 +8,16 @@
 //  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
 //  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
 const Emitter = require("EventEmitter")
-
+const EVENT_NAME = require("NAME_EVENT")
 cc.Class({
     extends: cc.Component,
-
     properties: {
         spritePrefab: cc.Prefab,
     },
-
-    // LIFE-CYCLE CALLBACKS:
-
     onLoad() {
-        this.node.on("mousedown", this.onMouseDown, this)
+        this.node.on("mousedown", this.onMouseDown, this);
+        this.mapController = this.node.getComponent("MapController")
         this.hasShotInTile = false;
-        this.clickCount = 1;
         this.mousePosition = cc.v2(0, 0);
         for (let i = 1; i < 82; i++) {
             let childNode = cc.instantiate(this.spritePrefab);
@@ -35,17 +31,15 @@ cc.Class({
             child.on(cc.Node.EventType.MOUSE_LEAVE, this.onLeftHoverChild, this);
         }
         //Emitter.instance.registerEvent("takePosition", this.onClick.bind(this));
-        cc.tween(this.node)
-            .to(2, {scaleX: 1.5})
-            .start()
-    },
 
+    },
     onClick(data) {
         if (this.hasShotInTile) return;
         if (this.node.getBoundingBoxToWorld().contains(data)) {
             let clickedChildNode = this.findClickedChildNode(this.node, data);
             if (clickedChildNode === null) return;
-            if (clickedChildNode.getComponent("Tile").hasShoot) return;
+            cc.log(this.mapController.clickCounter)
+            if (clickedChildNode.getComponent("Tile").hasShoot || this.mapController.clickCounter === 0) return;
             else {
                 let worldPosition = clickedChildNode.parent.convertToWorldSpaceAR(clickedChildNode.position);
                 cc.log("tile " + worldPosition)
@@ -56,24 +50,22 @@ cc.Class({
                 clickedChildNode.getComponent("Tile").hasShoot = true;
                 Emitter.instance.emit("spawnPrefab")
                 Emitter.instance.emit("attackToPosition", targetNode)
+                this.mapController.clickCounter = 0;
             }
 
         }
     },
-
     onHoverChild(data) {
         let isShoot = data.currentTarget.getComponent("Tile").hasShoot;
         if (!isShoot) data.currentTarget.color = cc.Color.GREEN;
 
     },
-
     onLeftHoverChild(data) {
         cc.log(data.currentTarget.getComponent("Tile").hasShoot);
         let isShoot = data.currentTarget.getComponent("Tile").hasShoot;
         if (isShoot) return;
         data.currentTarget.color = cc.Color.WHITE;
     },
-
     findClickedChildNode(parentNode, localPos) {
         for (let i = 0; i < parentNode.children.length; i++) {
             let childNode = parentNode.children[i];
@@ -83,16 +75,10 @@ cc.Class({
         }
         return null;
     },
-
     onMouseDown(event) {
-        if (event.getButton() !== cc.Event.EventMouse.BUTTON_LEFT || this.clickCount === 0) return;
+        if (event.getButton() !== cc.Event.EventMouse.BUTTON_LEFT) return;
         this.mousePosition = event.getLocation();
-        this.clickCount = 0;
         this.onClick(this.mousePosition)
         event.stopPropagation();
     },
-
-    onEnable(){
-        this.clickCount = 1;
-    }
 });
